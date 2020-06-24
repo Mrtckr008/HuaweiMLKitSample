2d34f0827f06196da3aa231cf70260d3c2f515ef
m00565027 <murat.cakir1@huawei.com>
2020-06-24 20:06:57 +0300

    Initial commit

---
+++ app/src/main/java/com/huawei/example/huaweimlkitsample/LandmarkRecognitionActivity.kt
@@ -0,0 +1,89 @@
+package com.huawei.example.huaweimlkitsample
+
+import android.graphics.Bitmap
+import android.location.Criteria
+import android.location.Geocoder
+import android.location.LocationManager
+import android.os.Bundle
+import android.text.Html
+import androidx.appcompat.app.AppCompatActivity
+import com.huawei.hmf.tasks.Task
+import com.huawei.hms.mlsdk.MLAnalyzerFactory
+import com.huawei.hms.mlsdk.common.MLFrame
+import com.huawei.hms.mlsdk.landmark.MLRemoteLandmark
+import com.huawei.hms.mlsdk.landmark.MLRemoteLandmarkAnalyzerSetting
+import kotlinx.android.synthetic.main.activity_landmark_recognization.*
+import java.text.DecimalFormat
+
+
+class LandmarkRecognitionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landmark_recognization)
+
+        val imageBitmap=intent.extras?.getString("imageBitmap") as Bitmap?
+        landmark_recognition_image.setImageBitmap(imageBitmap)
+        landmarkAnalyzer(imageBitmap!!)
+        println("mcmc image ->$imageBitmap")
+    }
+
+    private fun landmarkAnalyzer(imageBitmap: Bitmap) {
+        val settings = MLRemoteLandmarkAnalyzerSetting.Factory()
+            .setLargestNumOfReturns(1)
+            .setPatternType(MLRemoteLandmarkAnalyzerSetting.STEADY_PATTERN)
+            .create()
+        val analyzer = MLAnalyzerFactory.getInstance()
+            .getRemoteLandmarkAnalyzer(settings)
+        val mlFrame = MLFrame.Creator()
+            .setBitmap(imageBitmap).create()
+        val task: Task<List<MLRemoteLandmark>> =
+            analyzer.asyncAnalyseFrame(mlFrame)
+
+        task.addOnSuccessListener {
+            displaySuccess(it[0])
+        }.addOnFailureListener {
+           displayFailure()
+        } }
+
+    fun displaySuccess(landmark: MLRemoteLandmark) {
+        if (landmark.landmark.contains("retCode") || landmark.landmark
+                .contains("retMsg") || landmark.landmark.contains("fail")
+        ) {
+            landmark_information_text.text = "The landmark was not recognized."
+        } else {
+            var longitude = 0.0
+            var latitude = 0.0
+            var possibility = ""
+            var landmarkName = ""
+            var result = StringBuilder()
+            if (landmark.landmark != null) {
+                result = StringBuilder(
+                    """
+                        Landmark information
+                        ${landmark.landmark}
+                        """.trimIndent()
+                )
+                landmarkName = landmark.landmark
+            }
+            landmark_information_text.text = "LANDMARK INFORMATION : ${landmark.landmark}"
+
+            if (landmark.positionInfos != null) {
+                for (coordinate in landmark.positionInfos) {
+                    //           setText(Html.fromHtml("<b>" + myText + "</b>");
+                    result.append("\nLatitude: ").append(coordinate.lat)
+                    result.append("\nLongitude: ").append(coordinate.lng)
+                    result.append("\nPossibility: %")
+                        .append(DecimalFormat("##.##").format(landmark.possibility * 100))
+                    longitude = coordinate.lng
+                    latitude = coordinate.lat
+                    possibility = DecimalFormat("##.##").format(landmark.possibility * 100)
+                }
+            }
+            landmark_information_text.text = Html.fromHtml("<big><b>Landmark Information</b></big> <br><big><b>$landmarkName</b></big><br><b>Latitude: </b>$latitude<br><b>Longitude: </b>$longitude<br><b>Possibility: </b>%$possibility")
+        }
+    }
+    private fun displayFailure() {
+
+    }
+}
+
---
+++ app/src/main/java/com/huawei/example/huaweimlkitsample/MainActivity.kt
@@ -0,0 +1,104 @@
+package com.huawei.example.huaweimlkitsample
+
+import android.Manifest
+import android.R.attr
+import android.R.attr.bitmap
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+
+
+class MainActivity : AppCompatActivity() {
+    private val myCameraRequestCode = 100
+    private val myStorageRequestCode = 300
+    companion object{
+        var selectedImageBitmap:Bitmap?=null
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        this@MainActivity.intent = intent
+        intent!!.extras
+
+
+        take_picture_icon.setOnClickListener {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(Manifest.permission.CAMERA), myCameraRequestCode)
+            } else {
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, 100)
+            }
+        }
+
+        pick_image_icon.setOnClickListener {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), myStorageRequestCode)
+            } else {
+                val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(intent, 200)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 100) {
+            if (resultCode == Activity.RESULT_OK) { //  resim seçildiğinde yapılacaklar
+
+                selectedImageBitmap = if (data?.data == null) {
+                    data?.extras!!["data"] as Bitmap?
+                } else {
+                    MediaStore.Images.Media.getBitmap(
+                        this.contentResolver,
+                        data.data
+                    )
+                }
+                val intentFromCamera = Intent(this, LandmarkRecognitionActivity::class.java)
+                intent.putExtra("imageBitmap",selectedImageBitmap)
+                startActivity(intentFromCamera)
+            }
+        }
+        if (requestCode == 200) {
+            if (resultCode == Activity.RESULT_OK) {
+                val pickedImage: Uri? = data?.data
+                try {
+                    selectedImageBitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, pickedImage)
+                    val intentFromGallery = Intent(this, LandmarkRecognitionActivity::class.java)
+                    intentFromGallery.putExtra("imageBitmap", selectedImageBitmap)
+                    startActivity(intentFromGallery)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == myCameraRequestCode) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(intent, 100)
+            } else {
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+        else if(requestCode == myStorageRequestCode){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(intent, 200)
+            } else {
+                Toast.makeText(this, "Storage permission denied", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
